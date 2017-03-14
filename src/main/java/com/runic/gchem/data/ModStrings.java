3604bb3bda8750960cbe533ec0bc1aec26442b4a
RunicEnergy <myyyytosis@gmail.com>
2017-03-14 18:30:37 -0500

    Mod Class and Proxy Setup

---
+++ src/main/java/com/runic/gchem/data/ModStrings.java
@@ -0,0 +1,12 @@
+package com.runic.gchem.data;
+
+/**
+ * This file was born into existence by our overlord RunicEnergy on 14Mar2017 at 18:01.
+ */
+public class ModStrings {
+    public static final String MODID = "gchem";
+    public static final String MODNAME = "Grand Chemistry";
+    public static final String VERSION = "1.10.2-2185-1.0";
+    public static final String SERVER_PROXY = "com.runic.gchem.proxies.CommonProxy";
+    public static final String CLIENT_PROXY = "com.runic.gchem.proxies.ClientProxy";
+}
